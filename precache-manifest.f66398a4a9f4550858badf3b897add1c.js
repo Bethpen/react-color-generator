6de2276885332019731dbfdb60bfe139d2bc4ca2
Bethpen <64626802+Bethpen@users.noreply.github.com>
2021-07-31 23:24:41 +0100

    Updates

---
+++ precache-manifest.f66398a4a9f4550858badf3b897add1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c07c74f98853c28494dc59316ec6fc",
+    "url": "/react-color-generator/index.html"
+  },
+  {
+    "revision": "3ad8ea03e895fa15bd9b",
+    "url": "/react-color-generator/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "0b728d42613a7c9ec6d3",
+    "url": "/react-color-generator/static/js/2.cdd6e70d.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/react-color-generator/static/js/2.cdd6e70d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ad8ea03e895fa15bd9b",
+    "url": "/react-color-generator/static/js/main.71a477ec.chunk.js"
+  },
+  {
+    "revision": "86272262674149d27e8a",
+    "url": "/react-color-generator/static/js/runtime-main.9c11df16.js"
+  }
+]);
