9fecb03bac56a8842d93d2d1e7fe906bac7b58da
Bethpen <64626802+Bethpen@users.noreply.github.com>
2021-07-31 23:17:10 +0100

    first commit

---
+++ src/SingleColor.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import rgbToHex from './utils'
+
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+
+  const bcg = rgb.join(',');
+
+  useEffect(() => {
+    const alertFade = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => {
+      clearTimeout(alertFade);
+    }
+  }, [alert])
+
+  return <article className={`color ${index > 10 && `color-light`}`} style={{ backgroundColor: `rgb(${bcg})` }} onClick={(e) => {
+    setAlert(true);
+    navigator.clipboard.writeText(hexColor);
+  }} >
+    <p className="percent-value">{weight}%</p>
+    <p className="color-value">#{hexColor}</p>
+    {alert && <p className='alert'>copied to clipboard</p>}
+  </article>
+}
+
+export default SingleColor
